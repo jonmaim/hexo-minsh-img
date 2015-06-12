6fb487502eb31096f98b7566746c58b56421f1c6
Jon Maim <jonathan.maim@gmail.com>
2015-06-12 07:45:53 +0000

    stable

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # hexo-minsh-img
+This tag plugin will add a `max-width` style into the specified image. Look at the source if you want an example on how to access an asset path.
+
+## Usage
+```
+{% minsh_img trivago.jpg 'A post on the Trivago Facebook page' %}
+```
+will result into 
+```
+<p class="minsh-img"><img src="/community-building/3-steps-to-create-your-community-management-strategy-2-3/trivago.png" style="max-width:494px" alt="A post on the Trivago Facebook page" title="A post on the Trivago Facebook page"></p>
+```
--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 'use strict';
 
+var path = require('path');
+var gm = require('gm');
+var Promise = require('bluebird');
+
 var minshImg = function(args) {
-  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!', args);
-	var text = '<img src="' + args[0] + '" alt="' + args[1] + '" title="' + args[2] + '" />';
-	return text;
+  var PostAsset = hexo.model('PostAsset');
+
+  var slug = args[0];
+  if (!slug) { return; }
+
+  var asset = PostAsset.findOne({post: this._id, slug: slug});
+  if (!asset) { return; }
+
+  var altText = args.slice(1).join(' ');
+
+  var p = path.join(hexo.base_dir, asset._id);
+
+  return new Promise(function(resolve, reject){
+    gm(p).size(function (err, size) {
+      if (err) { return reject(err); }
+
+      return resolve('<p class="minsh-img"><img src="' + hexo.config.root + asset.path + '" style="max-width:' + size.width + 'px" alt="' + altText + '" title="' + altText + '" /></p>');
+    });
+  });
 };
 
-hexo.extend.tag.register('minsh_img', minshImg);
+hexo.extend.tag.register('minsh_img', minshImg, {async : true});
